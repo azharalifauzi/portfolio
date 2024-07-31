7a8d4644f82a23f3057d57de84f91ca0ce430d31
azharalifauzi <azharalifauzi@gmail.com>
2024-07-31 17:18:11 +0700

    feat: update tech stack

--- src/features/home/constants.ts
+++ src/features/home/constants.ts
@@ -4,7 +4,15 @@ export const HISTORY = [
     company: 'Piktochart',
     description:
       'Developed essential shape tools from scratch for a leading graphic design software, while resolving critical interaction bugs, including snap rotate, drag-and-drop functionality, page resizing, and copy-paste issues. <br /> <br /> Successfully migrated the TypeScript loader from ts-loader to swc-loader, reducing build time by 50%, from 15 minutes to 7 minutes. Additionally, revamped the Onboarding and Dashboard pages to enhance user experience and interface.',
-    techStacks: ['Vue', 'Nuxt', 'Typescript', 'SCSS', 'Webpack', 'Vite'],
+    techStacks: [
+      'Vue',
+      'Nuxt',
+      'Typescript',
+      'SCSS',
+      'Webpack',
+      'Vite',
+      'Tailwindcss',
+    ],
     url: 'https://piktochart.com',
   },
   {
@@ -12,7 +20,14 @@ export const HISTORY = [
     company: 'AIA Singapore',
     description:
       'Spearheaded the development of AIA Design System v2, creating robust and reusable React components that enhanced the efficiency and consistency of our projects. Provided mentorship to interns and junior engineers, fostering a collaborative and growth-oriented environment. <br /> <br /> Maintained and enhanced POSEZ, a crucial web application for submitting service requests for clients and insurance agents, consistently delivering improvements that boosted user satisfaction and operational efficiency.',
-    techStacks: ['React', 'Gatsby', 'Next.js', 'Javascript'],
+    techStacks: [
+      'React',
+      'Gatsby',
+      'Next.js',
+      'Javascript',
+      'Redux',
+      'Redux Saga',
+    ],
     url: 'https://www.aia.com.sg/en/index',
   },
   {
@@ -20,7 +35,7 @@ export const HISTORY = [
     company: 'PT Sirkel Kreasi Nusantara',
     description:
       'I started work as freelance and then hired as full time developer. Successfully delivered multiple projects utilizing diverse technologies such as React, Vue, AMP, and Laravel, showcasing versatility and adaptability in different development environments. <br /> <br /> Played a key role in developing the front end of a web-based banking system app for Bank Aladin Syariah using React.js, ensuring a user-friendly and efficient interface for seamless banking experiences.',
-    techStacks: ['HTML & CSS', 'Javascript', 'React', 'Vue'],
+    techStacks: ['HTML & CSS', 'Typescript', 'React', 'Vue', 'Ionic'],
     url: 'https://www.linkedin.com/company/sirkeli-id/?originalSubdomain=id',
   },
 ];

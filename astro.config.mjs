aae83a8adb4485d13111b0aaa6e88e3064d5e498
Azhar Ali Fauzi <azharalifauzi@gmail.com>
2024-08-31 21:03:13 +0700

    chore: add node adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
-
 import tailwind from '@astrojs/tailwind';
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    vue({
-      appEntrypoint: '/src/pages/_app',
-    }),
-    tailwind(),
-  ],
+  integrations: [vue({
+    appEntrypoint: '/src/pages/_app'
+  }), tailwind()],
+  output: "server",
+  adapter: node({
+    mode: "standalone"
+  })
 });
